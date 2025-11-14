5d340fe5a47781a28f47d2f8b5642c321c89f5ba
DiegoCoPi <diegoalexander.cp@gmail.com>
2025-11-14 14:34:37 -0500

    Calculadora mejorada

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
-def main():
-  print("Hello learners!")
+def addmultiplenumbers(numbers):
+    return sum(numbers)
 
-if __name__=="__main__":
-  main()
+
+def multiplymultiplenumbers(numbers):
+    result = 1
+    for n in numbers:
+        result *= n
+    return result
+
+
+def isiteven(num):
+    if isinstance(num, int) or (isinstance(num, float) and num.is_integer()):
+        return int(num) % 2 == 0
+    return False
+
+
+def isitaninteger(num):
+    if isinstance(num, int):
+        return True
+    if isinstance(num, float):
+        return num.is_integer()
+    return False
+
+
+def main():
+    print("Hello learners!")
+    nums = [2, 7, 8.5, 7, 9, 8, 30]
+
+
+if __name__ == "__main__":
+    main()
